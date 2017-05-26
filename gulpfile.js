7db4b7d4d68c4535f971f480203b5941d897543e
bill fero <billfero@gmail.com>
2017-05-26 13:57:43 +0100

    using gulp to concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
+/*
+	References:
+	[1] https://github.com/hdngr/treehouse-gulp-basics (course github repo)
+	[2] https://github.com/contra/gulp-concat
+	[3] https://github.com/dlmanning/gulp-sass
+
+	
+	To run the http-server
+	1. install globally
+		$ npm i http-server -g
+	2. run the server - specifying a port - default is 8080
+		$ http-server -p 3000
+ */
 'use strict';
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+
+
+// concat js files
+gulp.task('concat-scripts', () => {
+	gulp.src([
+		'js/jquery.js',
+		'js/sticky/jquery.sticky.js',
+		'js/main.js'])
+	.pipe(concat('app.js'))
+	.pipe(gulp.dest('js'));
+});
+
 
 gulp.task('hello', () => {
 	console.log('Hello from me ...');
@@ -9,6 +35,6 @@ gulp.task('goodbye', () => {
 	console.log('... and it\'s goodbye from him!')
 });
 
-gulp.task('default', ['hello', 'goodbye'], () => {
+gulp.task('default', ['hello', 'concat-scripts', 'goodbye'], () => {
 	console.log('... all tasks have been completed!');
 });
