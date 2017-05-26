220a8b7210b9bc4ebb8014d74e96827f3d05d6e0
bill fero <billfero@gmail.com>
2017-05-26 14:41:51 +0100

    compile sass into sass

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@
 	[4] https://github.com/hparra/gulp-rename
 	[5] https://github.com/terinjokes/gulp-uglify
 	[6] https://alistapart.com/article/better-javascript-minification
-	
+	[7] http://thesassway.com/intermediate/using-source-maps-with-sass
+	[8] https://github.com/gulp-sourcemaps/gulp-sourcemaps
+	[9] http://blog.teamtreehouse.com/introduction-source-maps
 	
 	To run the http-server
 	1. install globally
@@ -19,6 +21,7 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
 // concat js files - order matters!
 gulp.task('concat-scripts', () => {
@@ -38,6 +41,15 @@ gulp.task('minify-scripts', () => {
 		.pipe(gulp.dest('js'));
 });
 
+// compile sass
+gulp.task('compile-sass', () => {
+	// application.scss imports all other required scss files
+	gulp.src('scss/application.scss')
+		.pipe(sass())
+		.pipe(rename('app.css'))
+		.pipe(gulp.dest('css'))
+});
+
 gulp.task('hello', () => {
 	console.log('Hello from me ...');
 });
@@ -46,6 +58,6 @@ gulp.task('goodbye', () => {
 	console.log('... and it\'s goodbye from him!')
 });
 
-gulp.task('default', ['concat-scripts', 'minify-scripts'], () => {
+gulp.task('default', ['concat-scripts', 'minify-scripts', 'compile-sass'], () => {
 	console.log('... all tasks have been completed!');
 });
