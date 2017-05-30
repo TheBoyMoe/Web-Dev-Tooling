bc9439f6b1fdff74ced663ff071825ae6efa7046
bill fero <billfero@gmail.com>
2017-05-30 15:37:57 +0100

    working gulp prod/dev pipeline

--- gulp-useref/gulpfile.js
+++ gulp-useref/gulpfile.js
@@ -1,57 +1,66 @@
+/*
+	References:
+	[1] https://github.com/hdngr/treehouse-gulp-basics/tree/master (workshop repo)
+	[2] https://www.npmjs.com/package/gulp-useref (used to concat and rename files on the fly)
+	[3] https://github.com/jonkemp/gulp-useref/issues/163 (version 3 and using assets)
+
+ */
 "use strict";
 
-var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    maps = require('gulp-sourcemaps'),
-     del = require('del');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const maps = require('gulp-sourcemaps');
+const del = require('del');
+const useref = require('gulp-useref');
+const iff = require('gulp-if');
+const csso = require('gulp-csso');
 
-gulp.task("concatScripts", function() {
-    return gulp.src([
-        'js/jquery.js',
-        'js/sticky/jquery.sticky.js',
-        'js/main.js'
-        ])
-    .pipe(maps.init())
-    .pipe(concat('app.js'))
-    .pipe(maps.write('./'))
-    .pipe(gulp.dest('js'));
-});
-
-gulp.task("minifyScripts", ["concatScripts"], function() {
-  return gulp.src("js/app.js")
-    .pipe(uglify())
-    .pipe(rename('app.min.js'))
-    .pipe(gulp.dest('js'));
-});
+const options = {
+	src: 'src',
+	dist: 'dist'
+};
 
 gulp.task('compileSass', function() {
-  return gulp.src("scss/application.scss")
-      .pipe(maps.init())
-      .pipe(sass())
-      .pipe(maps.write('./'))
-      .pipe(gulp.dest('css'));
+	return gulp.src(options.src + "/scss/application.scss")
+		.pipe(maps.init())
+		.pipe(sass())
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest(options.src + '/css'));
 });
 
 gulp.task('watchFiles', function() {
-  gulp.watch('scss/**/*.scss', ['compileSass']);
-  gulp.watch('js/main.js', ['concatScripts']);
-})
-
-gulp.task('clean', function() {
-  del(['dist', 'css/application.css*', 'js/app*.js*']);
+	gulp.watch(options.src + '/scss/**/*.scss', ['compileSass']);
 });
 
-gulp.task("build", ['minifyScripts', 'compileSass'], function() {
-  return gulp.src(["css/application.css", "js/app.min.js", 'index.html',
-                   "img/**", "fonts/**"], { base: './'})
-            .pipe(gulp.dest('dist'));
+gulp.task('clean', function() {
+	del([
+		options.dist,
+		options.src + '/css/application.css*']);
+});
+
+gulp.task('html', ['compileSass'], () => {
+	let assets = useref.assets();
+	return gulp.src(options.src + '/index.html') // useref will use the css & js paths in the index.html file to know what files to process
+		.pipe(assets)
+		.pipe(iff('*.js', uglify())) // selectively minimise js files
+		.pipe(iff('*.css', csso())) // selectively minimise css files
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest(options.dist));
+});
+
+
+gulp.task('build', ['html'], () => {
+	return gulp.src([
+		options.src + '/img/**',
+		options.src + '/fonts/**'], { base: options.src})
+		.pipe(gulp.dest(options.dist));
 });
 
 gulp.task('serve', ['watchFiles']);
 
-gulp.task("default", ["clean"], function() {
-  gulp.start('build');
+gulp.task("default", ["clean"], () => {
+	gulp.start('build');
 });
